84c4d986207ae09114d1d2bca2d670c05f623535
RafaelBlazquez <73467706+RafaelBlazquez@users.noreply.github.com>
2021-09-28 13:57:23 +0200

    Add files via upload

---
+++ Animacion/Animacion.js
@@ -0,0 +1,13 @@
+
+        var posicionesX = [28.8,38.9,69.4];
+        var posicionesY = [16.1,31.7,41.8,51.9,62,72.1];
+        var currentX = 0;
+        var currentY = 0;
+        function go() {
+          moverValvula(3,1);
+         
+        }
+        function moverValvula(v1,v2) {
+            var valvula =document.getElementById("valvula");
+            valvula.set
+        }
